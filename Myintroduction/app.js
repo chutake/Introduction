d7608d81e7817c6e4eea077f772e1ef4aa8c4ad9
Takeru Chugun <takeru.chugun.3r@stu.hosei.ac.jp>
2022-06-01 02:27:42 +0900

    自己紹介

---
+++ Myintroduction/app.js
@@ -0,0 +1,33 @@
+const express=require('express');
+const app=express();
+const mysql=require('mysql');
+const PORT=3000;
+//パスの設定
+//これでviewディレクトリにいる
+//const path=require('path');
+//app.use(express.static(path.join(__dirname,'view')));
+//pubilcフォルダ内のファイルも読み込めるようにする。
+app.use(express.static('./public'));
+
+const connection=mysql.createConnection({
+    host: 'localhost',
+    user: 'viewer',
+    password: 'pass',
+    database: 'list_app'
+});
+
+console.log("サーバーを起動中\n");
+
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs');
+});
+
+app.get('/hello',(req,res)=>{
+    res.render('hello.ejs');
+});
+app.post('/create',(req,res)=>{
+    
+})
+
+app.listen(PORT);
